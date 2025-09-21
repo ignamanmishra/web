71aba494820b9b797b9125242c46716c32676591
Naman Mishra <136167538+ignamanmishra@users.noreply.github.com>
2025-09-21 14:47:25 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom']
+        }
+      }
+    }
+  },
   server: {
-    port: 3000
+    port: 3000,
+    open: true
+  },
+  preview: {
+    port: 3000,
+    open: true
   }
 })
